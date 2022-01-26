121110221845c5cb9d0b0270e453ef84e9ebea74
Iaroslav Krasnokutskii <Yroslavk1995@gmail.com>
2022-01-26 13:19:00 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Ярослав on 1/26/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
