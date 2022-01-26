96afd1734064fd7672a63b8b23e595f959ec1b28
Iaroslav Krasnokutskii <Yroslavk1995@gmail.com>
2022-01-26 16:43:26 +0300

    Working point

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -46,6 +46,7 @@ struct ContentView: View {
             //            }
             //            .font(.largeTitle)
             //            .padding(.horizontal)
+            //
             
         }
     }
